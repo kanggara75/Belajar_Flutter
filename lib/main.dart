9257910e55a1eaf790be8c980f4a3fff72d14d21
Call Vin <kanggara75@gmail.com>
2020-12-24 01:08:06 +0700

    menyelesaikan 006. Statefull && StateLess Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int i = 0;
+
+  void tekanTombol() {
+    setState(() {
+      i = i + 1;
+    });
+  }
+
+  void reset() {
+    setState(() {
+      i = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("05. Latihan Container"),
-        ),
-        body: Container(
-          color: Colors.red,
-          margin: EdgeInsets.all(10),
-          padding: EdgeInsets.all(10),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: <Color>[
-                  Colors.amber,
-                  Colors.blue,
-                ],
+        appBar: AppBar(title: Text("Statefull Demo")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                i.toString(),
+                style: TextStyle(
+                  fontSize: 10 + i.toDouble(),
+                ),
               ),
-            ),
+              RaisedButton(
+                child: Text("Tambah Angka"),
+                onPressed: tekanTombol,
+              ),
+              RaisedButton(
+                child: Text("Reset Angka"),
+                onPressed: reset,
+              ),
+            ],
           ),
         ),
       ),

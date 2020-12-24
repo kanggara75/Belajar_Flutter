27a8acfc755972a30c2cb15d0c30541a8479cbcd
Call Vin <kanggara75@gmail.com>
2020-12-24 13:51:59 +0700

    menyelesaikan 009. List && ListView

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,49 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<Widget> data = [];
+  int counter = 1;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Text Style")),
-        body: Center(
-          child: Text(
-            "Ini Text",
-            style: TextStyle(
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.bold,
-              fontSize: 40,
-              decoration: TextDecoration.overline,
-              decorationColor: Colors.red,
-              decorationThickness: 3,
-              decorationStyle: TextDecorationStyle.wavy,
+        appBar: AppBar(
+          title: Text("09. List && ListView"),
+        ),
+        body: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                RaisedButton(
+                  child: Text("Tambah Data"),
+                  onPressed: () {
+                    setState(() {
+                      data.add(Text(
+                        "Data Ke " + counter.toString(),
+                        style: TextStyle(fontSize: 50),
+                      ));
+                      counter++;
+                    });
+                  },
+                ),
+                RaisedButton(
+                  child: Text("Hapus Data"),
+                  onPressed: () {
+                    setState(() {
+                      data.removeLast();
+                      counter--;
+                    });
+                  },
+                ),
+              ],
             ),
-          ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: data,
+            ),
+          ],
         ),
       ),
     );

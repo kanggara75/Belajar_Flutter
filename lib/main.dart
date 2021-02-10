d6173430496bc0c261545f8a92dc2521d61f86b3
Call Vin <kanggara75@gmail.com>
2021-02-11 06:10:00 +0700

    kosong

--- lib/main.dart
+++ lib/main.dart
@@ -7,65 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("011. Flexibel Widget"),
-        ),
-        body: Column(
-          children: [
-            Flexible(
-              flex: 1,
-              child: Row(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: Container(
-                        margin: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: Colors.red,
-                        )),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: Container(
-                        margin: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(70),
-                          color: Colors.black,
-                        )),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: Container(
-                        margin: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: Colors.amber,
-                        )),
-                  ),
-                ],
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                  margin: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.blue,
-                  )),
-            ),
-            Flexible(
-              flex: 1,
-              child: Container(
-                  margin: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.green,
-                  )),
-            ),
-          ],
-        ),
+        appBar: AppBar(title: Text("012. Stack & Align Widget")),
       ),
     );
   }
